812f01a81cea78fca9a7376d8a74c961112d5013
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-12-21 02:45:30 -0800

    get project data is being throttled, so for large chunks of data, we retry with backoff

--- src/storage.ts
+++ src/storage.ts
@@ -30,21 +30,34 @@ export async function getProjectData(email: string | null, sourceType: SourceTyp
         }
     };
 
-    try {
-        const data = await dynamoDB.send(new GetCommand(params));
-        return data.Item ? data.Item.data : undefined;
-    } catch (error) {
-        console.error(`Error getting project data: ${error}`);
-        throw error;
+    let attempt = 0;
+    while (attempt < 5) {
+        try {
+            const data = await dynamoDB.send(new GetCommand(params));
+            return data.Item ? data.Item.data : undefined;
+        } catch (error: any) {
+            if (error.name === 'ProvisionedThroughputExceededException') {
+                console.error(`Attempt ${attempt + 1}: Throughput exceeded, retrying...`);
+                await sleep(2000 + attempt * 1000); // Exponential backoff
+            } else {
+                console.error(`Error getting project data: ${error}`);
+                throw error;
+            }
+        }
+        attempt++;
     }
+    throw new Error('Maximum retry attempts reached');    
 }
 
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 export async function storeProjectData(email: string | null, sourceType: SourceType, owner: string, project: string, resourcePath: string, analysisType: string, data: any): Promise<void> {
     const projectPath = `${email ? email : "public"}/${sourceType}/${owner}/${project}`;
     const dataPath = `${resourcePath}/${analysisType}`;
 
     const dataSize = Buffer.byteLength(JSON.stringify(data), 'utf8');
-    console.log(`storeProjectData for (${dataSize} bytes): ${projectPath}/${dataPath}`);
+    console.log(`storeProjectData for (${dataSize} bytes): ${projectPath}${dataPath}`);
 
     const params = {
         TableName: analysisDatastoreTableName,
