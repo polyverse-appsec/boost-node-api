43758a7162537081ffc1a51219c20a95920ba68b
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2024-01-05 22:19:57 -0800

    some minor restructuring to test auth in create_project data script

--- scripts/create_project.py
+++ scripts/create_project.py
@@ -1,9 +1,16 @@
 import argparse
 import requests
-import time
 import jwt
 import boto3
 import subprocess
+
+
+# REMOTE_URL = "https://pt5sl5vwfjn6lsr2k6szuvfhnq0vaxhl.lambda-url.us-west-2.on.aws"
+LOCAL_URL = "http://localhost:3000"
+BASE_URL = LOCAL_URL
+
+python_cmd = "python3"  # or "python"
+
 
 def get_private_key():
     secret_name = "boost-sara/sara-client-private-key"
@@ -25,89 +32,91 @@ def get_private_key():
 
     return private_key
 
-def create_project(email, organization, github_uri, project_name=None):
-    BASE_URL = "https://pt5sl5vwfjn6lsr2k6szuvfhnq0vaxhl.lambda-url.us-west-2.on.aws"
-    LOCAL_URL = "http://localhost:3000"
-    HEADERS = {'x-user-account': email}
 
+def get_headers(email):
     private_key = get_private_key()
 
-    expiration_unix_time = int(time.time()) + 60
-    unsignedIdentity = {"email": email, "expires": expiration_unix_time}
-    signedIdentity = jwt.encode(unsignedIdentity, private_key, algorithm='RS256')
-    signedHeaders = {'x-signed-identity': signedIdentity}
+    unsigned_identity = {"email": email}
+    signed_identity = jwt.encode(unsigned_identity, private_key, algorithm='RS256')
+    signed_headers = {'x-signed-identity': signed_identity}
 
-    data = {"resources": [github_uri]}
+    # unsignedHeaders = {'x-user-account': email}
 
-    response = requests.post(f"{BASE_URL}/api/user_project/{organization}/{project_name}", json=data, headers=HEADERS)
+    return signed_headers
+
+
+def create_project(email, organization, github_uri, project_name=None):
+
+    data = {"resources": [{"uri": github_uri}]}
+
+    response = requests.post(f"{BASE_URL}/api/user_project/{organization}/{project_name}", json=data, headers=get_headers(email))
     return response
+
 
 def run_script(summarizer_path, args):
     try:
-        print("python", summarizer_path, args)
-        result = subprocess.run(["python", summarizer_path, args], check=True, capture_output=True, text=True)
+        print(python_cmd, summarizer_path, args)
+        result = subprocess.run([python_cmd, summarizer_path, args], check=True, capture_output=True, text=True)
         print(f"Output of summarizer:\n{result.stdout}")
     except subprocess.CalledProcessError as e:
         print(f"Error running summarizer:\n{e.output}")
+
 
 def read_file(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
+
 def post_data(email, organization, project_name, resource_name, data):
-    BASE_URL = "https://pt5sl5vwfjn6lsr2k6szuvfhnq0vaxhl.lambda-url.us-west-2.on.aws"
-    LOCAL_URL = "http://localhost:3000"
-    HEADERS = {'x-user-account': email}
 
-    private_key = get_private_key()
-    expiration_unix_time = int(time.time()) + 60
-    unsigned_identity = {"email": email, "expires": expiration_unix_time}
-    signed_identity = jwt.encode(unsigned_identity, private_key, algorithm='RS256')
-    signed_headers = {'x-signed-identity': signed_identity}
-
-    payload = {"resources" : data}
+    payload = {"resources": data}
     response = requests.post(f"{BASE_URL}/api/user_project/{organization}/{project_name}/data/{resource_name}",
-                                data=payload, headers=HEADERS)
+                             data=payload, headers=get_headers(email))
     return response
 
+
 def post_data_references(email, organization, project_name):
-    BASE_URL = "https://pt5sl5vwfjn6lsr2k6szuvfhnq0vaxhl.lambda-url.us-west-2.on.aws"
-    LOCAL_URL = "http://localhost:3000"
-    HEADERS = {'x-user-account': email}
 
-    private_key = get_private_key()
-    expiration_unix_time = int(time.time()) + 60
-    unsigned_identity = {"email": email, "expires": expiration_unix_time}
-    signed_identity = jwt.encode(unsigned_identity, private_key, algorithm='RS256')
-    signed_headers = {'x-signed-identity': signed_identity}
-
-    post_response = requests.post(f"{BASE_URL}/api/user_project/{organization}/{project_name}/data_references/", headers=HEADERS)
+    post_response = requests.post(f"{BASE_URL}/api/user_project/{organization}/{project_name}/data_references/", headers=get_headers(email))
     if post_response.status_code != 200:
         print(f"Failed to process data references: {post_response.status_code}, {post_response.text}")
         return
 
     # GET request to retrieve processed data
-    get_response = requests.get(f"{BASE_URL}/api/user_project/{organization}/{project_name}/data_references/", headers=HEADERS)
+    get_response = requests.get(f"{BASE_URL}/api/user_project/{organization}/{project_name}/data_references/", headers=get_headers(email))
     if get_response.status_code == 200:
         print("Processed Data References!")
     else:
         print(f"Failed to retrieve data references: {get_response.status_code}, {get_response.text}")
-    
+
+
 def main():
     parser = argparse.ArgumentParser(description='Create a project with user info.')
-    parser.add_argument('email', type=str, help='Email of the user')
-    parser.add_argument('organization', type=str, help='Organization name')
-    parser.add_argument('github_uri', type=str, help='URI to GitHub repository')
-    parser.add_argument('path_to_summarizer', type=str, help='Path to summarizer folder')
-    parser.add_argument('--project_name', type=str, default=None, help='Project name (optional)')
+    parser.add_argument('--email', type=str, help='Email of the user')
+    parser.add_argument('--organization', nargs='?', type=str, help='Organization name')
+    parser.add_argument('--github_uri', type=str, help='URI to GitHub repository')
+    parser.add_argument('--path_to_summarizer', type=str, help='Path to summarizer folder')
+    parser.add_argument('--project_name', nargs='?', type=str, help='Project name (optional)')
     args = parser.parse_args()
 
     # Print the user information
     print(f"User Email: {args.email}")
+    if not args.organization:
+        # if no organization is provided, then we'll pull the organization name from github uri (e.g. polyverse-appsec)
+        args.organization = args.github_uri.split('/')[-2]
+    if not args.organization:
+        print("Organization not available.")
+        return
     print(f"Organization: {args.organization}")
     print(f"GitHub URI: {args.github_uri}")
-    if args.project_name:
-        print(f"Project Name: {args.project_name}")
+    if not args.project_name:
+        # if no project is provided, then we'll pull the repo name from github uri (e.g. summarizer)
+        # https://github.com/polyverse-appsec/summarizer
+        args.project_name = args.github_uri.split('/')[-1]
+    if not args.project_name:
+        print("Project name not available.")
+        return
+    print(f"Project Name: {args.project_name}")
 
     # Create the project
     response = create_project(args.email, args.organization, args.github_uri, args.project_name)
@@ -117,14 +126,14 @@ def main():
     if response.status_code == 200:
         print("Project created successfully. Running additional scripts...")
 
-        #run_script(args.path_to_summarizer, "--rawonly")
-        subprocess.run(["python", args.path_to_summarizer, "--rawonly"], check=True, capture_output=True, text=True)
+        # run_script(args.path_to_summarizer, "--rawonly")
+        subprocess.run([python_cmd, args.path_to_summarizer, "--rawonly"], check=True, capture_output=True, text=True)
         print("Raw files generated successfully.")
 
-        #run_script(args.path_to_summarizer, "")
-        subprocess.run(["python", args.path_to_summarizer], check=True, capture_output=True, text=True)
+        # run_script(args.path_to_summarizer, "")
+        subprocess.run([python_cmd, args.path_to_summarizer], check=True, capture_output=True, text=True)
         print("Processed summary files generated successfully.")
-        
+
         # Post files to openai
         for output_file, resource_name in [
             ("allfiles_combined.md", "projectsource"),
@@ -134,18 +143,17 @@ def main():
             post_response = post_data(args.email, args.organization, args.project_name, resource_name, file_content)
             print(f"POST to {resource_name}: {post_response.status_code}, {post_response.text}")
 
-
-        #placeholder blueprint code, replace the data with actual blueprint file
+        # placeholder blueprint code, replace the data with actual blueprint file
         blueprint_data = "resources"
         post_response = post_data(args.email, args.organization, args.project_name, "blueprint", blueprint_data)
         print(f"POST to blueprint: {post_response.status_code}, {post_response.text}")
-
 
         # Poke openai to process the files
         post_data_references(args.email, args.organization, args.project_name)
 
     else:
         print(f"Failed to create project. Server responded with: {response.status_code}, {response.text}")
+
 
 if __name__ == "__main__":
     main()
