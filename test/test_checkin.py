55a2b361c0eb5ede828eac2b26a8ee53e58b7244
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2024-01-22 22:42:24 -0800

    updated checkin test with new project status api

--- test/test_checkin.py
+++ test/test_checkin.py
@@ -1,10 +1,11 @@
 import unittest
 import requests
 import json
+import time
 
 from utils import get_signed_headers
 
-from constants import TARGET_URL, ORG, PREMIUM_EMAIL, PUBLIC_PROJECT, PRIVATE_PROJECT
+from constants import TARGET_URL, ORG, PREMIUM_EMAIL, PUBLIC_PROJECT, PRIVATE_PROJECT, EMAIL
 
 
 class BoostBackendCheckinSuite(unittest.TestCase):
@@ -46,19 +47,25 @@ class BoostBackendCheckinSuite(unittest.TestCase):
         self.assertFalse('title' in gettedData)
         self.assertFalse('details' in gettedData)
 
-    def test_user_project_resource_creation_public_project(self):
-        print("Running test: Create Project, Attach GitHub Resources")
+    def helper_test_user_project_resource_creation_project(self, private: bool):
+        if private:
+            print("Running test: Create Project, Attach GitHub Private Resources")
+        else:
+            print("Running test: Create Project, Attach GitHub Public Resources")
 
-        signedHeaders = get_signed_headers(PREMIUM_EMAIL)
-
-        project_name = "checkin_test"
-        public_git_project = PUBLIC_PROJECT
+        if not private:
+            signedHeaders = get_signed_headers(PREMIUM_EMAIL)
+            public_git_project = PRIVATE_PROJECT
+            project_name = "checkin_test_private_repo"
+        else:
+            signedHeaders = get_signed_headers(EMAIL)
+            public_git_project = PUBLIC_PROJECT
+            project_name = "checkin_test_public_repo"
 
         response = requests.delete(f"{TARGET_URL}/api/user_project/{ORG}/{project_name}", headers=signedHeaders)
         self.assertEqual(response.status_code, 200)
 
         project_data = {"resources": [{"uri": public_git_project}]}
-        signedHeaders = get_signed_headers(PREMIUM_EMAIL)
         response = requests.post(f"{TARGET_URL}/api/user_project/{ORG}/{project_name}", json=project_data, headers=signedHeaders)
         self.assertEqual(response.status_code, 200)
 
@@ -69,24 +76,25 @@ class BoostBackendCheckinSuite(unittest.TestCase):
         self.assertEqual(gotten_project_data['name'], project_name)
         self.assertEqual(gotten_project_data['resources'][0]['uri'], public_git_project)
 
+        # now we're going to loop every 15 seconds to see if the project status has completed synchronized - up to 120 seconds
+        # if it hasn't, we'll fail the test
+        for i in range(0, 8):
+            response = requests.get(f"{TARGET_URL}/api/user_project/{ORG}/{project_name}/status", headers=signedHeaders)
+            self.assertEqual(response.status_code, 200)
+
+            gotten_project_data = response.json()
+            if gotten_project_data['synchronized']:
+                print(f"Project is Fully Synchronized in {i * 15} seconds - {response.json()}")
+                break
+            else:
+                print(f"Project status is {gotten_project_data['status']}, waiting 15 seconds")
+                print(f"\tFull Project Status is {response.json()}")
+
+            # wait 15 seconds before probing again
+            time.sleep(15)
+
+    def test_user_project_resource_creation_public_project(self):
+        return self.helper_test_user_project_resource_creation_project(private=False)
+
     def test_user_project_resource_creation_private_project(self):
-        print("Running test: Create Project, Attach GitHub Resources")
-
-        signedHeaders = get_signed_headers(PREMIUM_EMAIL)
-
-        project_name = "checkin_test"
-        private_git_project = PRIVATE_PROJECT
-
-        response = requests.delete(f"{TARGET_URL}/api/user_project/{ORG}/{project_name}", headers=signedHeaders)
-        self.assertEqual(response.status_code, 200)
-
-        project_data = {"resources": [{"uri": private_git_project}]}
-        response = requests.post(f"{TARGET_URL}/api/user_project/{ORG}/{project_name}", json=project_data, headers=signedHeaders)
-        self.assertEqual(response.status_code, 200)
-
-        response = requests.get(f"{TARGET_URL}/api/user_project/{ORG}/{project_name}", headers=signedHeaders)
-        self.assertEqual(response.status_code, 200)
-
-        gotten_project_data = response.json()
-        self.assertEqual(gotten_project_data['name'], project_name)
-        self.assertEqual(gotten_project_data['resources'][0]['uri'], private_git_project)
+        return self.helper_test_user_project_resource_creation_project(private=True)
