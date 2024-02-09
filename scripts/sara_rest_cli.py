ae539744d591c49a0fd78ef0e9b47a5f731953ed
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2024-02-08 16:19:48 -0800

    improve sara rest cli

--- scripts/sara_rest_cli.py
+++ scripts/sara_rest_cli.py
@@ -33,12 +33,21 @@ def main(email, org, project, method):
     URL = LOCAL_URL  # Decide how you want to set this, maybe another CLI argument
     endpoints = {
         "status": f"{URL}/api/user_project/{org}/{project}/status",
-        "discovery": f"{URL}/api/user_project/{org}/{project}/discovery",
         "data_references": f"{URL}/api/user_project/{org}/{project}/data_references",
-        "projectsource_generator": f"{URL}/api/user_project/{org}/{project}/data/projectsource/generator",
-        "aispec_generator": f"{URL}/api/user_project/{org}/{project}/data/aispec/generator",
-        "blueprint_generator": f"{URL}/api/user_project/{org}/{project}/data/blueprint/generator",
-        "blueprint_generator_start": f"{URL}/api/user_project/{org}/{project}/data/blueprint/generator/start"
+
+        "discovery": f"{URL}/api/user_project/{org}/{project}/discovery",
+
+        "projectsource": f"{URL}/api/user_project/{org}/{project}/data/projectsource",
+        "aispec": f"{URL}/api/user_project/{org}/{project}/data/aispec",
+        "blueprint": f"{URL}/api/user_project/{org}/{project}/data/blueprint",
+
+        "projectsource_gen": f"{URL}/api/user_project/{org}/{project}/data/projectsource/generator",
+        "aispec_gen": f"{URL}/api/user_project/{org}/{project}/data/aispec/generator",
+        "blueprint_gen": f"{URL}/api/user_project/{org}/{project}/data/blueprint/generator",
+
+        "create_blueprint": f"{URL}/api/user_project/{org}/{project}/data/blueprint/generator/start",
+        "create_aispec": f"{URL}/api/user_project/{org}/{project}/data/aispec/generator/start",
+        "create_projectsource": f"{URL}/api/user_project/{org}/{project}/data/projectsource/generator/start"
     }
 
     if method not in endpoints:
@@ -46,11 +55,19 @@ def main(email, org, project, method):
         return
 
     url = endpoints[method]
-    response = make_request("GET" if method in ["data_references", "projectsource_generator", "aispec_generator", "blueprint_generator"] else "POST", url, email)
+    # if method starts with "create_" or is "discovery", then it's a POST request
+    response = make_request("POST" if method.startswith("create_") or method == "discovery" else "GET", url, email)
 
     # Output the response
     print(response.status_code)
-    print(response.text)
+
+    if (response.status_code != 200):
+        print(f"Failed({response.status_code}): {response.text}")
+    else:
+        if response.headers.get('content-type') == 'application/json':
+            print(response.json())
+        else:
+            print(response.text)
 
 
 if __name__ == "__main__":
@@ -58,7 +75,7 @@ if __name__ == "__main__":
     parser.add_argument("--email", required=True, help="The user's email address")
     parser.add_argument("--org", default="polyverse-appsec", help="The organization name (default: polyverse-appsec)")
     parser.add_argument("--project", required=True, help="The project name")
-    parser.add_argument("--method", required=True, choices=['status', 'discovery', 'data_references', 'projectsource', 'aispec', 'blueprint'], help="The method to run")
+    parser.add_argument("--method", default="status", choices=['status', 'discovery', 'data_references', 'projectsource', 'aispec', 'blueprint', 'blueprint_gen', 'projectsource_gen', 'aispec_gen'], help="The method to run")
     parser.add_argument("--stage", default="local", choices=['local', 'dev', 'test', 'prod'], help="The Service to target (default: local)")
 
     args = parser.parse_args()
