8cb2bf81098bf25a737f5334d61a5ee113d80d5b
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2024-03-06 14:43:07 -0800

    Fix mistake in http request options causing monitor failure

--- test/monitors/boost-api-dev-test.js
+++ test/monitors/boost-api-dev-test.js
@@ -16,10 +16,6 @@ const apiCanaryBlueprint = async function () {
         'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vbml0b3JAcG9seXRlc3QuYWkifQ.dLDYDOYTh8htRhw6eHaBMsF4jIPe23SBOybeyOoZ02g0ZTXGhX4yiV904-42YTiOzbD0sjRPWIJsi0ZAnxN9ln5YPKwPnjOnsgRDp82FxM0lsaitAD7_OaIMVA7sg8zzIWrTvTKPEfgsPtqDibI3pkL_IQwEHBmnGHZbv2UwX-39l6RWcU14S-QxdNVHB8U8Ux56SjupwD3GE31A2EtQnmK5DkcH2j6afOgu-o9WQpbLmPhfwtuvGrWcyRFpMzzsxkH_uFxoLBgd_YogeDNvKDGLk97J3T_Vz7tPKQSLKjhelRlVMg20Z-bmKWUxB9F7boc3wpbe4sixUJ34gD3k7Q'
     };
 
-    // Define the URL to be called
-    const url = 'https://3c27qu2ddje63mw2dmuqp6oa7u0ergex.lambda-url.us-west-2.on.aws/api/user/polyverse-appsec/account';
-    log.info("URL: " + url);
-
     // Diagnostic validation function
     const validateSuccessful = async function (response) {
         return new Promise((resolve, reject) => {
@@ -46,13 +42,33 @@ const apiCanaryBlueprint = async function () {
         });
     };
 
+    var stepConfig = {
+        'continueOnStepFailure': true
+    };
+
     // Diagnostic step to check internet connectivity and DNS resolution
     const diagnosticStep = async function() {
+        await synthetics.executeStep('Verify Step', async function (timeoutInMillis = 1000) {
+            console.log("Diagnostic Step completed", stepConfig);
+        });
+    };
+
+    // Execute diagnostic step
+    await diagnosticStep();
+
+    // Diagnostic step to check internet connectivity and DNS resolution
+    const diagnosticHttpStep = async function() {
+
+        let requestOptions = {
+            'hostname': 'httpbin.org',
+            'method': 'GET',
+            'path': '/get',
+            'port': 443,
+            'protocol': 'https:'
+        };
+
         const diagnosticUrl = 'https://httpbin.org/get'; // A known endpoint for testing
-        await synthetics.executeHttpStep('Diagnostic Step', {
-            method: 'GET',
-            url: diagnosticUrl,
-        }, async function(res) {
+        await synthetics.executeHttpStep('Diagnostic Http Step', requestOptions, async function(res) {
             let responseBody = '';
             res.on('data', (chunk) => responseBody += chunk);
             res.on('end', () => log.info('Diagnostic response body: ' + responseBody));
@@ -60,22 +76,30 @@ const apiCanaryBlueprint = async function () {
     };
 
     // Execute diagnostic step
-    await diagnosticStep();
+    await diagnosticHttpStep();
 
     // Diagnostic step to execute and log detailed request and response info
     console.log("Executing HTTP Step with diagnostics...");
-    
-    const stepParams = {
-        method: 'GET',
-        url: url,
-        headers: headers,
-    };
 
-    log.info("Step Parameters: " + JSON.stringify(stepParams));
+    
+    // Define the URL to be called
+    const url = 'https://3c27qu2ddje63mw2dmuqp6oa7u0ergex.lambda-url.us-west-2.on.aws/api/user/polyverse-appsec/account';
+    log.info("Account URL: " + url);
+
+    let accountParams = {
+        'hostname': '3c27qu2ddje63mw2dmuqp6oa7u0ergex.lambda-url.us-west-2.on.aws',
+        'method': 'GET',
+        'path': '/api/user/polyverse-appsec/account', // The path is the part of the URL after the hostname and before the querystring
+        'port': 443,
+        'protocol': 'https:',
+        'headers': headers,
+    };
+    
+    log.info("Account Step Parameters: " + JSON.stringify(accountParams));
 
     // Execute the HTTP request with detailed diagnostics
     try {
-        await synthetics.executeHttpStep('Verify Account Information', stepParams, validateSuccessful);
+        await synthetics.executeHttpStep('Verify Account Information', accountParams, validateSuccessful);
     } catch (error) {
         // Log any errors encountered during the request execution
         log.error('Request failed:', error);
